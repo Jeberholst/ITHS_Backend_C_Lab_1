e141ca06b421b8089516b61e6ac1d56f05b435c0
Joakim Eberholst <J_eberholst@hotmail.com>
2021-12-11 16:32:15 +0100

    Initial commit

---
+++ ITHS_Backend_C_Lab_1/Controllers/AnimalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITHS_Backend_C_Lab_1.Controllers;
+
+public class AnimalController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
